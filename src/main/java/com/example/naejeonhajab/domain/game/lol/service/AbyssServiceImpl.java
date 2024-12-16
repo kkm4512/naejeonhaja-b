dadd0bbede596858434b216a2342235814d20584
kkm4512 <nayoun440@naver.com>
2024-12-16 16:30:27 +0900

    refactor. 전체적인 래픽토링 1. 네이밍 리팩토링 2. 클래스,메서드별로 분할

--- src/main/java/com/example/naejeonhajab/domain/game/lol/service/AbyssServiceImpl.java
+++ src/main/java/com/example/naejeonhajab/domain/game/lol/service/AbyssServiceImpl.java
@@ -17,11 +17,17 @@ import com.example.naejeonhajab.domain.game.lol.entity.resultHistory.LolPlayerRe
 import com.example.naejeonhajab.domain.game.lol.entity.resultHistory.LolPlayerResultHistory;
 import com.example.naejeonhajab.domain.game.lol.entity.resultHistory.LolPlayerResultOutcome;
 import com.example.naejeonhajab.domain.game.lol.enums.LolType;
+import com.example.naejeonhajab.domain.game.lol.mapper.LolMapper;
 import com.example.naejeonhajab.domain.game.lol.repository.playerHistory.LolPlayerHistoryRepository;
 import com.example.naejeonhajab.domain.game.lol.repository.playerHistory.LolPlayerRepository;
 import com.example.naejeonhajab.domain.game.lol.repository.resultHistory.LolPlayerResultHistoryRepository;
 import com.example.naejeonhajab.domain.game.lol.repository.resultHistory.LolPlayerResultOutcomeRepository;
 import com.example.naejeonhajab.domain.game.lol.repository.resultHistory.LolPlayerResultRepository;
+import com.example.naejeonhajab.domain.game.lol.service.balancing.LolAssignServiceImpl;
+import com.example.naejeonhajab.domain.game.lol.service.balancing.LolBalanceServiceImpl;
+import com.example.naejeonhajab.domain.game.lol.service.balancing.LolCheckServiceImpl;
+import com.example.naejeonhajab.domain.game.lol.service.balancing.LolLineSortServiceImpl;
+import com.example.naejeonhajab.domain.game.lol.service.util.LolUtilService;
 import com.example.naejeonhajab.domain.user.entity.User;
 import com.example.naejeonhajab.security.AuthUser;
 import lombok.RequiredArgsConstructor;
@@ -42,16 +48,21 @@ import static com.example.naejeonhajab.common.response.enums.LolApiResponse.LOL_
 @RequiredArgsConstructor
 @Service("abyssServiceImpl")
 public class AbyssServiceImpl {
-    // Player
+    // PlayerRepository
     private final LolPlayerHistoryRepository lolPlayerHistoryRepository;
-    private final LolPlayerRepository lolPlayerRepository;
-
-    // Result
     private final LolPlayerResultHistoryRepository lolPlayerResultHistoryRepository;
-    private final LolPlayerResultRepository lolPlayerResultRepository;
-    private final LolPlayerResultOutcomeRepository lolPlayerResultOutcomeRepository;
 
-    private static int retries = 10000; // 최대 시도 횟수
+    // Mapper
+    private final LolMapper lolMapper;
+
+    // Balance
+    private final LolBalanceServiceImpl lolBalanceService;
+
+    // Util
+    private final LolUtilService lolUtilService;
+
+    // Variable
+    private static int retries = 10_000;
 
     // 10명의 인원으로, 5:5 팀 구성해준후 반환
     public LolTeamResponseDto createTeam(LolPlayerHistoryRequestDto dto) {
@@ -60,123 +71,38 @@ public class AbyssServiceImpl {
 
     // 로그인한 사용자 플레이어 히스토리 내역 저장
     @Transactional
-    public LolTeamResponseDto createPlayerHistoryAndTeam(LolPlayerHistoryRequestDto dto, AuthUser authUser) {
+    public LolTeamResponseDto createTeamAndSavePlayerHistory(LolPlayerHistoryRequestDto dto, AuthUser authUser) {
         User user = User.of(authUser);
         LolPlayerHistory playerHistory = LolPlayerHistory.from(dto,user, LolType.ABYSS);
-        lolPlayerHistoryRepository.save(playerHistory);
         List<LolPlayer> playerList = LolPlayer.from(dto,playerHistory);
-        lolPlayerRepository.saveAll(playerList);
+        lolMapper.insertPlayerHistory(playerHistory);
+        lolMapper.insertPlayers(playerList);
         return create(dto);
     }
 
     // 내전 결과 저장 메서드
     @Transactional
-    public void createResultTeam(LolPlayerResultHistoryRequestDto dto, AuthUser authUser) {
+    public void saveResultHistory(LolPlayerResultHistoryRequestDto dto, AuthUser authUser) {
         User user = User.of(authUser);
         LolPlayerResultHistory playerResultHistory = LolPlayerResultHistory.from(dto,user,LolType.ABYSS);
         lolPlayerResultHistoryRepository.save(playerResultHistory);
         LolPlayerResultOutcome playerResultOutcomeA = LolPlayerResultOutcome.from(dto.getTeamA(),playerResultHistory);
         LolPlayerResultOutcome playerResultOutcomeB = LolPlayerResultOutcome.from(dto.getTeamB(),playerResultHistory);
-        lolPlayerResultOutcomeRepository.saveAll(List.of(playerResultOutcomeA,playerResultOutcomeB));
         List<LolPlayerResult> playerResultsA = LolPlayerResult.from(dto.getTeamA(),playerResultOutcomeA);
         List<LolPlayerResult> playerResultsB = LolPlayerResult.from(dto.getTeamB(),playerResultOutcomeB);
-        lolPlayerResultRepository.saveAll(playerResultsA);
-        lolPlayerResultRepository.saveAll(playerResultsB);
-    }
-
-    // 팀을 랜덤하게 5명으로 나눔
-    public LolTeamResponseDto splitTeam(List<LolPlayerDto> riftPlayerRequestDtos) {
-        List<LolPlayerDto> teamA = new ArrayList<>();
-        List<LolPlayerDto> teamB = new ArrayList<>();
-        try {
-            Collections.shuffle(riftPlayerRequestDtos);
-            for ( int i=0; i<riftPlayerRequestDtos.size()/2; i++ ){
-                teamA.add(riftPlayerRequestDtos.get(i));
-                teamB.add(riftPlayerRequestDtos.get(i + (riftPlayerRequestDtos.size()/2)));
-            }
-        } catch (Exception e) {
-            throw new BaseException(BaseApiResponse.TEAM_MISMATCH);
-        }
-
-        return LolTeamResponseDto.of(teamA,teamB);
-    }
-
-    // 티어를 기준으로 5:5 팀 나누기
-    public LolTeamResponseDto generateBalanceByTier(LolTeamResponseDto team) {
-        List<LolPlayerDto> bestTeamA = new ArrayList<>();
-        List<LolPlayerDto> bestTeamB = new ArrayList<>();
-        try {
-            List<LolPlayerDto> allPlayers = new ArrayList<>();
-            allPlayers.addAll(team.getTeamA());
-            allPlayers.addAll(team.getTeamB());
-            // 모든 조합 탐색
-            int n = allPlayers.size() / 2;
-            List<List<LolPlayerDto>> allCombinations = generateCombinations(allPlayers, n);
-
-            int minDifference = Integer.MAX_VALUE;
-
-
-            // 각 조합에 대해 점수 차이를 계산
-            for (List<LolPlayerDto> teamA : allCombinations) {
-                List<LolPlayerDto> teamB = new ArrayList<>(allPlayers);
-                teamB.removeAll(teamA);
-
-                int teamA_score = calculateScore(teamA);
-                int teamB_score = calculateScore(teamB);
-
-                int difference = Math.abs(teamA_score - teamB_score);
-                if (difference < minDifference) {
-                    minDifference = difference;
-                    bestTeamA = new ArrayList<>(teamA);
-                    bestTeamB = new ArrayList<>(teamB);
-                }
-            }
-
-        } catch (Exception e) {
-            throw new BaseException(BaseApiResponse.TEAM_MISMATCH);
-        }
-        // 가장 밸런스가 좋은 팀 반환
-        return LolTeamResponseDto.of(bestTeamA, bestTeamB);
-    }
-
-    // 각 팀의 mmr 총합 계산
-    private int calculateScore(List<LolPlayerDto> team) {
-        return team.stream()
-                .mapToInt(LolPlayerDto::getMmr)
-                .sum();
-    }
-
-    // 두 팀중 최적의 mmr점수 차이가 적은 조합을 찾는 메서드
-    private List<List<LolPlayerDto>> generateCombinations(List<LolPlayerDto> players, int size) {
-        List<List<LolPlayerDto>> combinations = new ArrayList<>();
-        generateCombinationsHelper(players, new ArrayList<>(), 0, size, combinations);
-        return combinations;
-    }
-
-    // 두 팀중 최적의 mmr점수 차이가 적은 조합을 찾는 메서드 실질적으로 이행하는 메서드
-    private void generateCombinationsHelper(List<LolPlayerDto> players, List<LolPlayerDto> current, int index, int size, List<List<LolPlayerDto>> result) {
-        if (current.size() == size) {
-            result.add(new ArrayList<>(current));
-            return;
-        }
-        if (index == players.size()) return;
-
-        // 현재 플레이어 포함
-        current.add(players.get(index));
-        generateCombinationsHelper(players, current, index + 1, size, result);
-
-        // 현재 플레이어 제외
-        current.remove(current.size() - 1);
-        generateCombinationsHelper(players, current, index + 1, size, result);
+        lolMapper.inserPlayerResultHistory(playerResultHistory);
+        lolMapper.insertPlayerResultOutcome(List.of(playerResultOutcomeA,playerResultOutcomeB));
+        lolMapper.insertPlayersResult(playerResultsA);
+        lolMapper.insertPlayersResult(playerResultsB);
     }
 
     // 10명의 유저를 받아, 5:5대전팀을 만들어주는 공통 메서드
     public LolTeamResponseDto create(LolPlayerHistoryRequestDto dto) {
-        initMmr(dto.getLolPlayerDtos());
+        lolUtilService.initMmr(dto.getLolPlayerDtos());
         while (retries > 0) {
             try {
-                LolTeamResponseDto one = splitTeam(dto.getLolPlayerDtos());
-                return generateBalanceByTier(one);
+                LolTeamResponseDto one = lolUtilService.splitTeam(dto.getLolPlayerDtos());
+                return lolBalanceService.generateBalanceByTier(one);
             } catch (Exception e) {
                 retries--;
             }
@@ -184,34 +110,27 @@ public class AbyssServiceImpl {
         throw new BaseException(BaseApiResponse.TEAM_MISMATCH);
     }
 
-    // 10명의 유저에 대해 티어에 따른 MMR 부여 메서드
-    private void initMmr(List<LolPlayerDto> riftRequestDtos){
-        for (LolPlayerDto riftPlayerRequestDto : riftRequestDtos) {
-            riftPlayerRequestDto.updateMmr(riftPlayerRequestDto.getTier().getScore());
-        }
-    }
-
     // 특정 ID의 플레이어 상세 히스토리 반환 (단일)
-    public LolPlayerHistoryResponseDetailDto getDetailTeam(Long playerHistoryId) {
+    public LolPlayerHistoryResponseDetailDto getPlayerHistoryDetailTeam(Long playerHistoryId) {
         LolPlayerHistory lolPlayerHistory = findLolPlayerHistoryByPlayerHistoryId(playerHistoryId);
         return LolPlayerHistoryResponseDetailDto.of(lolPlayerHistory);
     }
 
     // 특정 ID의 플레이어 대전 상세 내역 반환 (단일)
-    public LolPlayerResultHistoryResponseDetailDto getDetailResultTeam(Long playerResultHistoryId) {
+    public LolPlayerResultHistoryResponseDetailDto getResultHistoryDetailTeam(Long playerResultHistoryId) {
         LolPlayerResultHistory lolPlayerResultHistory = findLolPlayerResultHistoryByPlayerHistoryId(playerResultHistoryId);
         return LolPlayerResultHistoryResponseDetailDto.of(lolPlayerResultHistory);
     }
 
     // 현재 유저가 가지고있는 플레이어 히스토리 내역 조회 (다건)
-    public Page<LolPlayerHistoryResponseSimpleDto> getSimpleTeam(AuthUser authUser, Pageable pageable) {
+    public Page<LolPlayerHistoryResponseSimpleDto> getPlayerHistorySimpleTeam(AuthUser authUser, Pageable pageable) {
         User user = User.of(authUser);
         return findLolPlayerHistoryByUser(user, pageable)
                 .map(LolPlayerHistoryResponseSimpleDto::of);
     }
 
     // 현재 유저가 가지고있는 대전 상세 내역 조회 (다건)
-    public Page<LolPlayerResultHistoryResponseSimpleDto> getSimpleResultTeam(AuthUser authUser, Pageable pageable) {
+    public Page<LolPlayerResultHistoryResponseSimpleDto> getResultHistorySimpleTeam(AuthUser authUser, Pageable pageable) {
         User user = User.of(authUser);
         return findLolPlayerResultHistoryByUser(user, pageable)
                 .map(LolPlayerResultHistoryResponseSimpleDto::of);
