efc3a5c4f4be44c942ed38e7fe17894681c265db
kkm4512 <nayoun440@naver.com>
2024-12-17 20:30:00 +0900

    fix. 클라이언트 도매인으로 직접 지정

--- src/main/java/com/example/naejeonhajab/domain/user/service/UserService.java
+++ src/main/java/com/example/naejeonhajab/domain/user/service/UserService.java
@@ -59,11 +59,12 @@ public class UserService {
         String jwt = jm.generateJwt(JwtDto.of(user));
 
         ResponseCookie cookie = ResponseCookie.from(AUTHORIZATION_HEADER, jwt)
-                .path("/")                  // 쿠키의 경로 설정
-                .secure(true)   // HTTPS 연결만 허용
+                .path("/")
+                .secure(true)
+                .domain("naejeonhaja.com")
                 .sameSite("None")
                 .httpOnly(false)
-                .maxAge(7 * 24 * 60 * 60)   // 쿠키 만료 시간 (7일)
+                .maxAge(7 * 24 * 60 * 60)
                 .build();
 
         response.addHeader("Set-Cookie", cookie.toString());
